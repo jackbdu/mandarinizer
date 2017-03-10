15db1de1e08778da8620d90afc4bd65f03614dcf
JackBDu <jd2804@nyu.edu>
2017-03-10 17:30:30 +0800

    argument parser added for player

--- manvidplayer.py
+++ manvidplayer.py
@@ -1,49 +1,52 @@
 #!/usr/bin/env python
 # encoding=utf-8
 
-import time # sleep
-import sys  # exit
+import time     # sleep
+import sys      # exit
+import argparse # argparse
 
-__author__ = "Jack B. Du"
-__copyright__ = "Copyright (c) 2017, Jack B. Du"
-__license__ = "MIT"
-__email__ = "jackbdu@nyu.edu"
+__author__ = 'Jack B. Du'
+__copyright__ = 'Copyright (c) 2017, Jack B. Du'
+__license__ = 'MIT'
+__email__ = 'jackbdu@nyu.edu'
 
-# define the output file name here
-filename = "output.manvid"
+parser = argparse.ArgumentParser()
+parser.add_argument('filename', metavar='filename', type=str)
+args = parser.parse_args()
 
 frames = []
-def load_file(filename, frames_to_return):
+def loadFile(filename, frames):
     try:
-        manvidfile = open(filename, "r")
+        manvidfile = open(filename, 'r')
     except IOError:
-        print "no such file or directory: " + filename
+        print 'no such file or directory: ' + filename
         sys.exit()
 
-    filetype, image_height = manvidfile.readline().split(',')
+    filetype, imageHeight = manvidfile.readline().split(',')
 
     while manvidfile:
-        print("get")
-        frame = ""
+        print('get')
+        frame = ''
         line = manvidfile.readline()
-        if line == "":
+        if line == '':
             break
-        for i in range(int(image_height)-1):
+        for i in range(int(imageHeight)-1):
             frame += line
             line = manvidfile.readline()
         frame += line
-        frames_to_return.append(frame)
+        frames.append(frame)
 
     manvidfile.close()
 
-load_file("output.manvid", frames)
+print 'loading file: '+args.filename
+loadFile(args.filename, frames)
 
 try:
     while True:
         for frame in frames:
-            print "1"
+            print '1'
             # clear the terminal window
-            print(chr(27) + "[2J")
+            print(chr(27) + '[2J')
             # print the frame
             print frame
             time.sleep(0.2)
