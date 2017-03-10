2de828c5fb8f7b687d294cb39ff63ccebdfa7fd7
JackBDu <jd2804@nyu.edu>
2017-03-10 18:31:41 +0800

    player comments added

--- manvidplayer.py
+++ manvidplayer.py
@@ -10,52 +10,70 @@ __copyright__ = 'Copyright (c) 2017, Jack B. Du'
 __license__ = 'MIT'
 __email__ = 'jackbdu@nyu.edu'
 
+# parsing arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('filename', metavar='filename', type=str)
 args = parser.parse_args()
 
+# buffer that stores all the frames of texts
 framesBuffer = []
 
-def loadFile(filename):
+# load frames from the file
+def load_frames(filename):
+    # initialize empty list for frames to return
     frames = []
     try:
+        # open file for reading
         manvidfile = open(filename, 'r')
+    # error when opening file
     except IOError:
         print 'no such file or directory: ' + filename
         sys.exit()
 
-    filetype, frameWidthStr, frameHeightStr = manvidfile.readline().split(',')
+    # read first line in file
+    line = manvidfile.readline()
 
-    while manvidfile:
+    # get meta data from the file (first line in the file, seperated by ','
+    filetype, frameWidthStr, frameHeightStr = line.split(',')
+
+    # while file not finished
+    while line != '':
+        # initialize an empty list for storing frame
         frame = ''
-        line = manvidfile.readline()
-        if line == '':
-            break
-        for i in range(int(frameHeightStr)-1):
-            frame += line
+        # load one frame
+        for i in range(int(frameHeightStr)):
             line = manvidfile.readline()
-        frame += line
+            # add current line to frame
+            frame += line
+        # add current frame to frames
         frames.append(frame)
 
+    # close the file
     manvidfile.close()
 
     return frames, int(frameWidthStr), int(frameHeightStr)
 
+# clear the terminal window
+print(chr(27) + '[2J')
 print 'loading...'
-framesBuffer, frameWidth, frameHeight = loadFile(args.filename)
+framesBuffer, frameWidth, frameHeight = load_frames(args.filename)
+# get the length (number) of frames
 framesLength = len(framesBuffer)
+
 try:
+    # repeat frames
     while True:
+        # play frames
         for i in range(framesLength):
             # clear the terminal window
             print(chr(27) + '[2J')
             # print the frame
             print framesBuffer[i]
+            # progress bar
             print frameWidth*i/framesLength*'田'+(frameWidth-frameWidth*i/framesLength)*'囗'
+            # time for one frame
             time.sleep(0.2)
 
 # handle KeyboardInterrupt, typically Ctrl + C
 except KeyboardInterrupt:
-    print frameWidth
-    print framesLength
     sys.exit()
